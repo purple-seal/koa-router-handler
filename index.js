d3b03ba381dd964a92ef47cb9b384586d8b61e92
boungkwan <purple0seal@gmail.com>
2018-10-04 15:59:49 +0900

    Add checking validation

--- index.js
+++ index.js
@@ -4,7 +4,30 @@ class HttpError {
     this.message = message
   }
 }
-function wrapHandler (handler) {
+
+function hasError (key, validation, params) {
+  const value = params[key]
+  return (validation.required && key in params === false
+    || validation.type && typeof value !== validation.type
+    || validation.pattern && !validation.pattern.test(value))
+}
+
+function createErrorMsg (key, validation, params) {
+  const value = params[key]
+  if (validation.required && key in params === false) {
+     return `Required field missing: ${key}`
+  }
+  if (validation.type && typeof value !== validation.type) {
+    return `The value ${key} is not expected type: ${validation.type}.`
+  }
+  if (validation.pattern && !validation.pattern.test(value)) {
+    return `The value ${key} has not expected pattern: ${validation.pattern}.`
+  }
+
+  return false
+}
+
+function wrapHandler (handler, { validations, createErrorBody } = {}) {
   return async function controllerHandler (ctx) {
     const method = ctx.method.toUpperCase()
     let params
@@ -21,6 +44,16 @@ function wrapHandler (handler) {
     }
 
     try {
+      if (validations) {
+        const key = Object.keys(validations).find(k => hasError(k, validations[k], params))
+        if (key) {
+          const msg = createErrorMsg(key, validations[key], params)
+          ctx.status = 400
+          ctx.body = createErrorBody ? createErrorBody(key, { status: 400, message: msg }) : msg
+          return
+        }
+      }
+
       ctx.body = await handler(params)
     } catch (e) {
       if (process.env.NODE_ENV !== 'production') {
@@ -30,8 +63,9 @@ function wrapHandler (handler) {
         ctx.status = e.status
         ctx.body = e.message
       } else {
+        const message = 'Internal server error'
         ctx.status = 500
-        ctx.body = 'Internal server error'
+        ctx.body = createErrorBody ? createErrorBody(null, { status: 500, message }) : message
       }
     }
   }
--- index.test.js
+++ index.test.js
@@ -1,6 +1,75 @@
-function wrapController(handler) {
+const { wrap } = require('./index')
 
-  return function (ctx, next) {
+describe('wrapper for router handler', () => {
+  test('create body with query parameter', async () => {
+    const handler = wrap((params) => ({ data: params['a'] }))
+    const ctx = {
+      method: 'GET',
+      query: { 'a': 'OK' }
+    }
+    await handler(ctx)
 
-  }
-}
+    expect(ctx.body).toHaveProperty('data', 'OK')
+  })
+
+  test('check validation of required field', async () => {
+    const handler = wrap((params) => ({ data: params['a'] }), {
+      validations: {
+        b: {
+          required: true
+        }
+      }
+    })
+    const ctx = {
+      method: 'GET',
+      query: { 'a': 'OK' }
+    }
+    await handler(ctx)
+
+    expect(ctx.status).toEqual(400)
+    expect(ctx.body).toEqual('Required field missing: b')
+  })
+
+  test('check validation of type', async () => {
+    const handler = wrap((params) => ({ data: params['a'] }), {
+      validations: {
+        a: {
+          type: 'number'
+        }
+      }
+    })
+    const ctx = {
+      method: 'GET',
+      query: { 'a': 'OK' }
+    }
+    await handler(ctx)
+
+    expect(ctx.status).toEqual(400)
+    expect(ctx.body).toEqual('The value a is not expected type: number.')
+  })
+
+  test('check validation with "createErrorBody"', async () => {
+    const handler = wrap((params) => ({ data: params['a'] }), {
+      validations: {
+        b: {
+          required: true
+        }
+      },
+      createErrorBody: (key, { status, message}) => ({
+        code: 102,
+        message
+      })
+    })
+    const ctx = {
+      method: 'GET',
+      query: { 'a': 'OK' }
+    }
+    await handler(ctx)
+
+    expect(ctx.status).toEqual(400)
+    expect(ctx.body).toHaveProperty('code', 102)
+    expect(ctx.body).toHaveProperty('message', 'Required field missing: b')
+  })
+
+})
+
